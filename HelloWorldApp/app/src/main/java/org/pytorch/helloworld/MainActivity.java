670547b958a7037557c530027a666b6edb1bcef7
Ivan Kobzarev <ivankobzarev@fb.com>
2019-10-01 15:06:51 -0700

    [app] Hello World app for tutorials

---
+++ HelloWorldApp/app/src/main/java/org/pytorch/helloworld/MainActivity.java
@@ -0,0 +1,99 @@
+package org.pytorch.helloworld;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import org.pytorch.IValue;
+import org.pytorch.Module;
+import org.pytorch.Tensor;
+import org.pytorch.torchvision.TensorImageUtils;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+
+    Bitmap bitmap = null;
+    Module module = null;
+    try {
+      // creating bitmap from packaged into app android asset 'image.jpg',
+      // app/src/main/assets/image.jpg
+      bitmap = BitmapFactory.decodeStream(getAssets().open("image.jpg"));
+      // loading serialized torchscript module from packaged into app android asset model.pt,
+      // app/src/model/assets/model.pt
+      module = Module.load(assetFilePath(this, "model.pt"));
+    } catch (IOException e) {
+      Log.e("PytorchHelloWorld", "Error reading assets", e);
+      finish();
+    }
+
+    // showing image on UI
+    ImageView imageView = findViewById(R.id.image);
+    imageView.setImageBitmap(bitmap);
+
+    // preparing input tensor
+    final Tensor inputTensor = TensorImageUtils.bitmapToFloat32Tensor(bitmap,
+        TensorImageUtils.TORCHVISION_NORM_MEAN_RGB, TensorImageUtils.TORCHVISION_NORM_STD_RGB);
+
+    // running the model
+    final Tensor outputTensor = module.forward(IValue.tensor(inputTensor)).getTensor();
+
+    // getting tensor content as java array of floats
+    final float[] scores = outputTensor.getDataAsFloatArray();
+
+    // searching for the index with maximum score
+    float maxScore = -Float.MAX_VALUE;
+    int maxScoreIdx = -1;
+    for (int i = 0; i < scores.length; i++) {
+      if (scores[i] > maxScore) {
+        maxScore = scores[i];
+        maxScoreIdx = i;
+      }
+    }
+
+    String className = ImageNetClasses.IMAGENET_CLASSES[maxScoreIdx];
+
+    // showing className on UI
+    TextView textView = findViewById(R.id.text);
+    textView.setText(className);
+  }
+
+  /**
+   * Copies specified asset to the file in /files app directory and returns this file absolute path.
+   *
+   * @return absolute file path
+   */
+  public static String assetFilePath(Context context, String assetName) throws IOException {
+    File file = new File(context.getFilesDir(), assetName);
+    if (file.exists() && file.length() > 0) {
+      return file.getAbsolutePath();
+    }
+
+    try (InputStream is = context.getAssets().open(assetName)) {
+      try (OutputStream os = new FileOutputStream(file)) {
+        byte[] buffer = new byte[4 * 1024];
+        int read;
+        while ((read = is.read(buffer)) != -1) {
+          os.write(buffer, 0, read);
+        }
+        os.flush();
+      }
+      return file.getAbsolutePath();
+    }
+  }
+}
