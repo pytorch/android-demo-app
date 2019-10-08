d9e4bb788cec7adc03bb38c5f4fd3ad20fe150f9
Ivan Kobzarev <ivankobzarev@fb.com>
2019-10-07 18:36:58 -0700

    [demo] Add moving average

--- PyTorchDemoApp/app/src/main/java/org/pytorch/demo/InfoViewFactory.java
+++ PyTorchDemoApp/app/src/main/java/org/pytorch/demo/InfoViewFactory.java
@@ -8,18 +8,31 @@ import android.widget.TextView;
 import androidx.annotation.Nullable;
 
 public class InfoViewFactory {
-  public static final int INFO_VIEW_TYPE_IMAGE_CLASSIFICATION = 1;
-  public static final int INFO_VIEW_TYPE_TEXT_CLASSIFICATION = 2;
+  public static final int INFO_VIEW_TYPE_IMAGE_CLASSIFICATION_RESNET = 1;
+  public static final int INFO_VIEW_TYPE_IMAGE_CLASSIFICATION_QMOBILENET = 2;
+  public static final int INFO_VIEW_TYPE_TEXT_CLASSIFICATION = 3;
 
   public static View newInfoView(Context context, int infoViewType, @Nullable String additionalText) {
     LayoutInflater inflater = LayoutInflater.from(context);
-    if (INFO_VIEW_TYPE_IMAGE_CLASSIFICATION == infoViewType) {
+    if (INFO_VIEW_TYPE_IMAGE_CLASSIFICATION_RESNET == infoViewType) {
       View view = inflater.inflate(R.layout.info, null, false);
       TextView infoTextView = view.findViewById(R.id.info_title);
       TextView descriptionTextView = view.findViewById(R.id.info_description);
 
       infoTextView.setText(R.string.vision_card_resnet_title);
       StringBuilder sb = new StringBuilder(context.getString(R.string.vision_card_resnet_description));
+      if (additionalText != null) {
+        sb.append('\n').append(additionalText);
+      }
+      descriptionTextView.setText(sb.toString());
+      return view;
+    } else if (INFO_VIEW_TYPE_IMAGE_CLASSIFICATION_QMOBILENET == infoViewType) {
+      View view = inflater.inflate(R.layout.info, null, false);
+      TextView infoTextView = view.findViewById(R.id.info_title);
+      TextView descriptionTextView = view.findViewById(R.id.info_description);
+
+      infoTextView.setText(R.string.vision_card_qmobilenet_title);
+      StringBuilder sb = new StringBuilder(context.getString(R.string.vision_card_qmobilenet_description));
       if (additionalText != null) {
         sb.append('\n').append(additionalText);
       }
--- PyTorchDemoApp/app/src/main/java/org/pytorch/demo/vision/VisionListActivity.java
+++ PyTorchDemoApp/app/src/main/java/org/pytorch/demo/vision/VisionListActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 
 import org.pytorch.demo.AbstractListActivity;
+import org.pytorch.demo.InfoViewFactory;
 import org.pytorch.demo.R;
 
 public class VisionListActivity extends AbstractListActivity {
@@ -13,12 +14,17 @@ public class VisionListActivity extends AbstractListActivity {
     super.onCreate(savedInstanceState);
     findViewById(R.id.vision_card_qmobilenet_click_area).setOnClickListener(v -> {
       final Intent intent = new Intent(VisionListActivity.this, ImageClassificationActivity.class);
-      intent.putExtra(ImageClassificationActivity.INTENT_MODULE_ASSET_NAME, "mobilenet_quantized_scripted_925.pt");
+      intent.putExtra(ImageClassificationActivity.INTENT_MODULE_ASSET_NAME,
+          "mobilenet_quantized_scripted_925.pt");
+      intent.putExtra(ImageClassificationActivity.INTENT_INFO_VIEW_TYPE,
+          InfoViewFactory.INFO_VIEW_TYPE_IMAGE_CLASSIFICATION_QMOBILENET);
       startActivity(intent);
     });
     findViewById(R.id.vision_card_resnet_click_area).setOnClickListener(v -> {
       final Intent intent = new Intent(VisionListActivity.this, ImageClassificationActivity.class);
       intent.putExtra(ImageClassificationActivity.INTENT_MODULE_ASSET_NAME, "resnet18.pt");
+      intent.putExtra(ImageClassificationActivity.INTENT_INFO_VIEW_TYPE,
+          InfoViewFactory.INFO_VIEW_TYPE_IMAGE_CLASSIFICATION_RESNET);
       startActivity(intent);
     });
   }
