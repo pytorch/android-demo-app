1c9e7f3824c96a78614f4a0d806f6fa4d96aa062
Akshit Khurana <axit@fb.com>
2020-10-21 16:49:32 -0700

    Add optimize_for_mobile call and update model

--- HelloWorldApp/trace_model.py
+++ HelloWorldApp/trace_model.py
@@ -1,8 +1,10 @@
 import torch
 import torchvision
+from torch.utils.mobile_optimizer import optimize_for_mobile
 
 model = torchvision.models.resnet18(pretrained=True)
 model.eval()
 example = torch.rand(1, 3, 224, 224)
 traced_script_module = torch.jit.trace(model, example)
-traced_script_module.save("app/src/main/assets/model.pt")
+torchscript_model_optimized = optimize_for_mobile(traced_script_module)
+torchscript_model_optimized.save("app/src/main/assets/model.pt")
