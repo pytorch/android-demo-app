a95114eb4f438b49ede2f0f3757a023ee40adaa4
Jeff Tang <jeffxtang@fb.com>
2020-11-01 10:42:05 -0800

    make inference run on worker thread

--- ImageSegmentation/app/src/main/java/org/pytorch/imagesegmentation/MainActivity.java
+++ ImageSegmentation/app/src/main/java/org/pytorch/imagesegmentation/MainActivity.java
@@ -23,8 +23,9 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.Map;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements Runnable {
+    private ImageView imageView;
+    private Button buttonSegment;
     private Bitmap bitmap = null;
     private Module module = null;
     private static final String imagename = "deeplab.jpg";
@@ -66,7 +67,7 @@ public class MainActivity extends AppCompatActivity {
             finish();
         }
 
-        final ImageView imageView = findViewById(R.id.imageView);
+        imageView = findViewById(R.id.imageView);
         imageView.setImageBitmap(bitmap);
 
         final Button buttonRestart = findViewById(R.id.restartButton);
@@ -85,61 +86,70 @@ public class MainActivity extends AppCompatActivity {
         });
 
 
-        final Button buttonSegment = findViewById(R.id.segmentButton);
+        buttonSegment = findViewById(R.id.segmentButton);
         buttonSegment.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 buttonSegment.setEnabled(false);
-                try {
-                    module = Module.load(MainActivity.assetFilePath(getApplicationContext(), "deeplabv3_scripted.pt"));
-                } catch (IOException e) {
-                    Log.e("ImageSegmentation", "Error reading assets", e);
-                    finish();
-                }
-                final Tensor inputTensor = TensorImageUtils.bitmapToFloat32Tensor(bitmap,
-                        TensorImageUtils.TORCHVISION_NORM_MEAN_RGB, TensorImageUtils.TORCHVISION_NORM_STD_RGB);
 
-                Map<String, IValue> outTensors = module.forward(IValue.from(inputTensor)).toDictStringKey();
-                final Tensor outputTensor = outTensors.get("out").toTensor();
+                Thread thread = new Thread(MainActivity.this);
+                thread.start();
+            }
+        });
+    }
 
-                final float[] scores = outputTensor.getDataAsFloatArray();
-
-                int width = bitmap.getWidth();
-                int height = bitmap.getHeight();
-                int[] intValues = new int[width * height];
-                for (int i = 0; i < intValues.length; i++) {
-                    intValues[i] = 0xFFFFFFFF;
-                }
-
-                for (int j = 0; j < width; j++) {
-                    for (int k = 0; k < height; k++) {
-                        int maxj = 0;
-                        int maxk = 0;
-                        int maxi = 0;
-
-                        double maxnum = -100000.0;
-                        for (int i=0; i < CLASSNUM; i++) {
-                            if (scores[i*(width*height) + j*width + k] > maxnum) {
-                                maxnum = scores[i*(width*height) + j*width + k];
-                                maxj = j; maxk= k; maxi = i;
-                            }
-                        }
-
-                        if (maxi == PERSON)
-                            intValues[maxj*width + maxk] = 0xFFFF0000;
-                        else if (maxi == DOG)
-                            intValues[maxj*width + maxk] = 0xFF00FF00;
-                        else if (maxi == SHEEP)
-                            intValues[maxj*width + maxk] = 0xFF0000FF;
-                        else
-                            intValues[maxj*width + maxk] = 0xFF000000;
+    @Override
+    public void run() {
+        try {
+            module = Module.load(MainActivity.assetFilePath(getApplicationContext(), "deeplabv3_scripted.pt"));
+        } catch (IOException e) {
+            Log.e("ImageSegmentation", "Error reading assets", e);
+            finish();
+        }
+        final Tensor inputTensor = TensorImageUtils.bitmapToFloat32Tensor(bitmap,
+                TensorImageUtils.TORCHVISION_NORM_MEAN_RGB, TensorImageUtils.TORCHVISION_NORM_STD_RGB);
+        Map<String, IValue> outTensors = module.forward(IValue.from(inputTensor)).toDictStringKey();
+        final Tensor outputTensor = outTensors.get("out").toTensor();
+        final float[] scores = outputTensor.getDataAsFloatArray();
+        int width = bitmap.getWidth();
+        int height = bitmap.getHeight();
+        int[] intValues = new int[width * height];
+        for (int i = 0; i < intValues.length; i++) {
+            intValues[i] = 0xFFFFFFFF;
+        }
+        for (int j = 0; j < width; j++) {
+            for (int k = 0; k < height; k++) {
+                int maxj = 0;
+                int maxk = 0;
+                int maxi = 0;
+                double maxnum = -100000.0;
+                for (int i = 0; i < CLASSNUM; i++) {
+                    if (scores[i * (width * height) + j * width + k] > maxnum) {
+                        maxnum = scores[i * (width * height) + j * width + k];
+                        maxj = j;
+                        maxk = k;
+                        maxi = i;
                     }
                 }
+                if (maxi == PERSON)
+                    intValues[maxj * width + maxk] = 0xFFFF0000;
+                else if (maxi == DOG)
+                    intValues[maxj * width + maxk] = 0xFF00FF00;
+                else if (maxi == SHEEP)
+                    intValues[maxj * width + maxk] = 0xFF0000FF;
+                else
+                    intValues[maxj * width + maxk] = 0xFF000000;
+            }
+        }
 
-                Bitmap bmpSegmentation = Bitmap.createScaledBitmap(bitmap, width, height, true);
-                Bitmap outputBitmap = bmpSegmentation.copy( bmpSegmentation.getConfig() , true);
-                outputBitmap.setPixels(intValues, 0, outputBitmap.getWidth(), 0, 0, outputBitmap.getWidth(), outputBitmap.getHeight());
-                Bitmap mTransferredBitmap = Bitmap.createScaledBitmap(outputBitmap, bitmap.getWidth(), bitmap.getHeight(), true);
-                imageView.setImageBitmap(mTransferredBitmap);
+        Bitmap bmpSegmentation = Bitmap.createScaledBitmap(bitmap, width, height, true);
+        Bitmap outputBitmap = bmpSegmentation.copy(bmpSegmentation.getConfig(), true);
+        outputBitmap.setPixels(intValues, 0, outputBitmap.getWidth(), 0, 0, outputBitmap.getWidth(), outputBitmap.getHeight());
+        final Bitmap transferredBitmap = Bitmap.createScaledBitmap(outputBitmap, bitmap.getWidth(), bitmap.getHeight(), true);
+
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                imageView.setImageBitmap(transferredBitmap);
                 buttonSegment.setEnabled(true);
             }
         });
