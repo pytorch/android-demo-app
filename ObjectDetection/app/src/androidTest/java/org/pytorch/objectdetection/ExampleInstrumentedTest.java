671e154a13ce0597d5156b90ab1377178076b3a6
Jeff Tang <jeffxtang@fb.com>
2020-11-19 18:22:30 -0800

    initial commit

---
+++ ObjectDetection/app/src/androidTest/java/org/pytorch/objectdetection/ExampleInstrumentedTest.java
@@ -0,0 +1,32 @@
+// Copyright (c) 2020 Facebook, Inc. and its affiliates.
+// All rights reserved.
+//
+// This source code is licensed under the BSD-style license found in the
+// LICENSE file in the root directory of this source tree.
+
+package org.pytorch.objectdetection;
+
+import android.content.Context;
+
+import androidx.test.platform.app.InstrumentationRegistry;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@RunWith(AndroidJUnit4.class)
+public class ExampleInstrumentedTest {
+    @Test
+    public void useAppContext() {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
+        assertEquals("org.pytorch.objectdetection", appContext.getPackageName());
+    }
+}
