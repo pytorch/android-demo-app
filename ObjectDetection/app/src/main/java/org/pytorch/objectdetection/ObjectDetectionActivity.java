b1a3f1b7573a1fbf254358c594fed3b5e3f1e077
Jeff Tang <jeffxtang@fb.com>
2020-11-23 18:05:00 -0800

    50% done on the live object detection

--- ObjectDetection/app/src/main/java/org/pytorch/objectdetection/ObjectDetectionActivity.java
+++ ObjectDetection/app/src/main/java/org/pytorch/objectdetection/ObjectDetectionActivity.java
@@ -1,27 +1,42 @@
 package org.pytorch.objectdetection;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.ImageFormat;
+import android.graphics.Rect;
+import android.graphics.YuvImage;
+import android.media.Image;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.TextureView;
 import android.view.ViewStub;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.annotation.WorkerThread;
 import androidx.camera.core.ImageProxy;
 
+import org.pytorch.IValue;
+import org.pytorch.Module;
+import org.pytorch.Tensor;
+import org.pytorch.torchvision.TensorImageUtils;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.util.ArrayList;
+
 public class ObjectDetectionActivity extends AbstractCameraXActivity<ObjectDetectionActivity.AnalysisResult> {
+    private static float[] NO_MEAN_RGB = new float[] {0.0f, 0.0f, 0.0f};
+    public static float[] NO_STD_RGB = new float[] {1.0f, 1.0f, 1.0f};
+
+
+    private Module mModule = null;
+
+
     static class AnalysisResult {
-
-        private final String[] topNClassNames;
-        private final float[] topNScores;
-        private final long analysisDuration;
-        private final long moduleForwardDuration;
-
         public AnalysisResult(String[] topNClassNames, float[] topNScores,
                               long moduleForwardDuration, long analysisDuration) {
-            this.topNClassNames = topNClassNames;
-            this.topNScores = topNScores;
-            this.moduleForwardDuration = moduleForwardDuration;
-            this.analysisDuration = analysisDuration;
         }
     }
 
@@ -40,6 +55,7 @@ public class ObjectDetectionActivity extends AbstractCameraXActivity<ObjectDetec
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
     }
 
@@ -48,12 +64,68 @@ public class ObjectDetectionActivity extends AbstractCameraXActivity<ObjectDetec
     }
 
 
+    private Bitmap imgToBitmap(Image image) {
+        Image.Plane[] planes = image.getPlanes();
+        ByteBuffer yBuffer = planes[0].getBuffer();
+        ByteBuffer uBuffer = planes[1].getBuffer();
+        ByteBuffer vBuffer = planes[2].getBuffer();
+
+        int ySize = yBuffer.remaining();
+        int uSize = uBuffer.remaining();
+        int vSize = vBuffer.remaining();
+
+        byte[] nv21 = new byte[ySize + uSize + vSize];
+        //U and V are swapped
+        yBuffer.get(nv21, 0, ySize);
+        vBuffer.get(nv21, ySize, vSize);
+        uBuffer.get(nv21, ySize + vSize, uSize);
+
+        YuvImage yuvImage = new YuvImage(nv21, ImageFormat.NV21, image.getWidth(), image.getHeight(), null);
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        yuvImage.compressToJpeg(new Rect(0, 0, yuvImage.getWidth(), yuvImage.getHeight()), 75, out);
+
+        byte[] imageBytes = out.toByteArray();
+        return BitmapFactory.decodeByteArray(imageBytes, 0, imageBytes.length);
+    }
+
     @Override
     @WorkerThread
     @Nullable
     protected AnalysisResult analyzeImage(ImageProxy image, int rotationDegrees) {
-        // TODO: inference
-        //MainActivity.getModule();
+
+        try {
+            if (mModule == null) {
+                mModule = Module.load(MainActivity.assetFilePath(getApplicationContext(), "yolov5s.torchscript.pt"));
+            }
+            Bitmap bitmap = imgToBitmap(image.getImage());
+            Bitmap resizedBitmap = Bitmap.createScaledBitmap(bitmap, PrePostProcessor.inputWidth, PrePostProcessor.inputHeight, true);
+            final Tensor inputTensor = TensorImageUtils.bitmapToFloat32Tensor(resizedBitmap, NO_MEAN_RGB, NO_STD_RGB);
+
+            final float[] inputs = inputTensor.getDataAsFloatArray();
+            IValue[] outputTuple = mModule.forward(IValue.from(inputTensor)).toTuple();
+            final Tensor outputTensor = outputTuple[0].toTensor();
+            final float[] outputs = outputTensor.getDataAsFloatArray();
+
+            final ArrayList<Result> results =  PrePostProcessor.outputsToNMSPredictions(outputs, 1.0f, 1.0f, 1.0f, 1.0f, 0, 0);
+
+
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    for (Result result : results) {
+                        Toast.makeText(getApplicationContext(), ""+result.classIndex, Toast.LENGTH_SHORT);
+                    }
+                }
+            });
+
+        }
+        catch (IOException e) {
+            Log.e("Object Detection", "Error reading assets", e);
+            finish();
+        }
+
+
+
         return null;
     }
 
