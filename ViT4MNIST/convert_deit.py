69e37c6b59bb5d675a8881da22d1f341403f1fb0
Jeff Tang <jeffxtang@fb.com>
2021-01-08 17:46:20 -0800

    use of quantization for FB DEIT and update of README

--- ViT4MNIST/convert_deit.py
+++ ViT4MNIST/convert_deit.py
@@ -2,6 +2,7 @@ import torch
 from torch.utils.mobile_optimizer import optimize_for_mobile
 
 model = torch.hub.load('facebookresearch/deit:main', 'deit_base_patch16_224', pretrained=True)
-ts_model = torch.jit.script(model)
+quantized_model = torch.quantization.quantize_dynamic(model, qconfig_spec={torch.nn.Linear}, dtype=torch.qint8)
+ts_model = torch.jit.script(quantized_model)
 optimized_torchscript_model = optimize_for_mobile(ts_model)
 optimized_torchscript_model.save("fbdeit.pt")
