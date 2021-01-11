40df659c0f02a67aed9f45475b022f3602e82652
热心网友丁同学 <2280131807@qq.com>
2021-01-11 14:57:38 +0800

    修改控件

--- PyTorchDemoApp/app/src/main/java/org/pytorch/demo/SideBarActivity.java
+++ PyTorchDemoApp/app/src/main/java/org/pytorch/demo/SideBarActivity.java
@@ -1,11 +1,16 @@
 package org.pytorch.demo;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.res.ColorStateList;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -24,11 +29,18 @@ import org.pytorch.demo.nlp.NLPListActivity;
 import org.pytorch.demo.vision.VisionListActivity;
 
 public class SideBarActivity extends AppCompatActivity {
+
+
+//    private  SharedPreferences sp = getSharedPreferences("info", Context.MODE_PRIVATE);
+
+    @SuppressLint("SetTextI18n")
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
+        Intent i = this.getIntent();
+        String info = i.getStringExtra("info");
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sidemenu);
-
+        View view = this.getLayoutInflater().inflate(R.layout.activity_sidemenu,null);
+        setContentView(view);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         NavigationView navigationview = (NavigationView) findViewById(R.id.navigation_view);
         final DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
@@ -46,40 +58,32 @@ public class SideBarActivity extends AppCompatActivity {
         //添加头布局的另外一种方式
         //View headview=navigationview.inflateHeaderView(R.layout.navigationview_header);
 
-        //寻找头部里面的控件
-        ImageView imageView = headerView.findViewById(R.id.iv_head);
-        imageView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(getApplicationContext(), "点击了头像", Toast.LENGTH_LONG).show();
-            }
-        });
-        navigationview.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
-                return false;
-            }
-        });
-        ColorStateList csl = (ColorStateList) getResources().getColorStateList(R.color.nav_menu_text_color);
+
+        navigationview.setNavigationItemSelectedListener(menuItem -> false);
+        ColorStateList csl = getResources().getColorStateList(R.color.nav_menu_text_color);
         //设置item的条目颜色
         navigationview.setItemTextColor(csl);
         //去掉默认颜色显示原来颜色  设置为null显示本来图片的颜色
         navigationview.setItemIconTintList(csl);
 
+        //登录组件传值
+        TextView msg = navigationview.findViewById(R.id.cname);
+        msg.setText(info);
 
+
+        //寻找头部里面的控件
+        androidx.appcompat.widget.AppCompatImageView imageView = view.findViewById(R.id.iv_head);
+        imageView.setOnClickListener(v -> Toast.makeText(getApplicationContext(), msg.getText(), Toast.LENGTH_LONG).show());
 
         //设置条目点击监听
-        navigationview.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
-                //安卓
-                Toast.makeText(getApplicationContext(), menuItem.getTitle(), Toast.LENGTH_LONG).show();
-                //设置哪个按钮被选中
-                menuItem.setChecked(true);
-                //关闭侧边栏
-                drawer.closeDrawers();
-                return false;
-            }
+        navigationview.setNavigationItemSelectedListener(menuItem -> {
+            //安卓
+            Toast.makeText(getApplicationContext(), menuItem.getTitle(), Toast.LENGTH_LONG).show();
+            //设置哪个按钮被选中
+            menuItem.setChecked(true);
+            //关闭侧边栏
+            drawer.closeDrawers();
+            return false;
         });
 
         findViewById(R.id.main_vision_click_view).setOnClickListener(v -> startActivity(new Intent(SideBarActivity.this, VisionListActivity.class)));
@@ -90,13 +94,11 @@ public class SideBarActivity extends AppCompatActivity {
     public void SetupMenu(View view) {
         startActivity(new Intent(SideBarActivity.this,SetupMenuActivity.class));
     }
+
+    //退出
+    public void exit(View view){
+        startActivity(new Intent(SideBarActivity.this,WelcomeActivity.class));
+
+    }
 }
 
-
-
-
-
-//设置单个消息数量
-//        LinearLayout llAndroid = (LinearLayout) navigationview.getMenu().findItem(R.id.single_1).getActionView();
-//        TextView msg= (TextView) navigationview.getMenu().findItem(R.id.msg_bg);
-//        msg.setText("李龙");
--- PyTorchDemoApp/app/src/main/java/org/pytorch/demo/WelcomeActivity.java
+++ PyTorchDemoApp/app/src/main/java/org/pytorch/demo/WelcomeActivity.java
@@ -3,10 +3,12 @@ package org.pytorch.demo;
 
 import android.Manifest;
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.os.Parcelable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -34,16 +36,11 @@ public class WelcomeActivity extends AppCompatActivity implements View.OnClickLi
     private EditText mEtPassword;
     private Toast mtoast;
 
-
-    //    private ViewPager mViewPager;
-//    private PagerAdapter mViewPagerAdapter;
-//    private TabLayout mTabLayout;
     private Button online_loginbtn;
 //    private Button offline_loginbtn;
 
     @Override
     public void onPointerCaptureChanged(boolean hasCapture) {
-
     }
 
     private static class PageData {
@@ -57,21 +54,7 @@ public class WelcomeActivity extends AppCompatActivity implements View.OnClickLi
             this.descriptionTextResId = descriptionTextResId;
         }
     }
-//
-//    private static final PageData[] PAGES = new PageData[] {
-//            new PageData(
-//                    R.string.welcome_page_title,
-//                    R.drawable.ic_logo_pytorch,
-//                    R.string.welcome_page_description),
-//            new PageData(
-//                    R.string.welcome_page_image_classification_title,
-//                    R.drawable.ic_image_classification_l,
-//                    R.string.welcome_page_image_classification_description),
-//            new PageData(
-//                    R.string.welcome_page_nlp_title,
-//                    R.drawable.ic_text_classification_l,
-//                    R.string.welcome_page_nlp_description)
-//    };
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -83,7 +66,7 @@ public class WelcomeActivity extends AppCompatActivity implements View.OnClickLi
         mEtUser = findViewById(R.id.et_1);
         mEtPassword = findViewById(R.id.et_2);
         offLogin.setOnClickListener(this);
-
+        saveUser();
 
         online_loginbtn = findViewById(R.id.btn_onlinelogin);
         //重写点击事件的处理方法onClick()
@@ -92,7 +75,6 @@ public class WelcomeActivity extends AppCompatActivity implements View.OnClickLi
             Toast.makeText(getApplicationContext(), "你点击了按钮", Toast.LENGTH_SHORT).show();
         });
 
-        saveuser();
 
 //        findViewById(R.id.skip_button).setOnClickListener(v -> startActivity(new Intent(WelcomeActivity.this, MainActivity.class)));
 //
@@ -112,69 +94,38 @@ public class WelcomeActivity extends AppCompatActivity implements View.OnClickLi
         Intent intent = null;
         if (username.equals("") || password.equals("")) {
             //账户密码不能为空
-            showToast("账号密码不能为空");
+            showToast("账号或密码不能为空");
         }
-        String savedUsername = sp.getString("username","");
-        String savedPassword = sp.getString("password","");
+        String savedUsername = sp.getString("username", "");
+        String savedPassword = sp.getString("password", "");
         if (username.equals(savedUsername) && password.equals(savedPassword)) {
-            showToast("登陆成功！");
+
             intent = new Intent(WelcomeActivity.this, SideBarActivity.class);
+            String res = sp.getString("username","none");
+            intent.putExtra("info", res);
+            showToast("你好！"+res);
             startActivity(intent);
         } else {
             showToast("用户名或密码错误，请重新登录");
+
         }
+
     }
 
 
-//    private class WelcomeViewPagerAdapter extends PagerAdapter {
-//        @Override
-//        public int getCount() {
-//            return PAGES.length;
-//        }
-//
-//        @Override
-//        public boolean isViewFromObject(@NonNull View view, @NonNull Object object) {
-//            return object == view;
-//        }
 
-//        @NonNull
-//        public Object instantiateItem(@NonNull ViewGroup container, int position) {
-//            final LayoutInflater inflater = LayoutInflater.from(WelcomeActivity.this);
-//            final View pageView = inflater.inflate(R.layout.welcome_pager_page, container, false);
-//            final TextView titleTextView = pageView.findViewById(R.id.welcome_pager_page_title);
-//            final TextView descriptionTextView = pageView.findViewById(R.id.welcome_pager_page_description);
-//            final ImageView imageView = pageView.findViewById(R.id.welcome_pager_page_image);
-//
-//            final PageData pageData = PAGES[position];
-//            titleTextView.setText(pageData.titleTextResId);
-//            descriptionTextView.setText(pageData.descriptionTextResId);
-//            imageView.setImageResource(pageData.imageResId);
-//            container.addView(pageView);
-//            return pageView;
-//        }
-
-//        @Override
-//        public void destroyItem(@NonNull ViewGroup container, int position, @NonNull Object object) {
-//            container.removeView((View) object);
-//        }
-//    }
-
-
-    private void showToast(String msg){
-        mtoast=Toast.makeText(WelcomeActivity.this,msg,Toast.LENGTH_SHORT);
+    private void showToast(String msg) {
+        mtoast = Toast.makeText(WelcomeActivity.this, msg, Toast.LENGTH_SHORT);
         mtoast.show();
     }
 
-    private void saveuser() {
+    private void saveUser() {
         // TODO Auto-generated method stub
-        sp =getSharedPreferences("info", Activity.MODE_PRIVATE);
-        SharedPreferences.Editor edit=sp.edit();
+        sp = getSharedPreferences("info", Activity.MODE_PRIVATE);
+        SharedPreferences.Editor edit = sp.edit();
         edit.putString("username", "admin");
         edit.putString("password", "123456");
-        edit.apply();
+        edit.commit();
     }
 
-//        public void get_sidebar(View view) {
-//            startActivity(new Intent(WelcomeActivity.this, SideBarActivity.class));
-//        }
 }
