746fc51a5609f4abdd4e3d20b1b0de693d7e167d
丁豪 <2280131807@qq.com>
2021-03-08 10:30:36 +0800

    UI

--- PyTorchDemoApp/app/src/main/java/org/pytorch/demo/ContentfuncActivity.java
+++ PyTorchDemoApp/app/src/main/java/org/pytorch/demo/ContentfuncActivity.java
@@ -3,18 +3,20 @@ package org.pytorch.demo;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
-//import android.support.v4.app.Fragment;
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+
 import android.widget.RadioButton;
 
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-import java.util.Objects;
+
 
 public class ContentfuncActivity extends Fragment {
 
@@ -34,6 +36,19 @@ public class ContentfuncActivity extends Fragment {
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
+
+        RadioButton local_recog = getActivity().findViewById(R.id.local_recog);
+        RadioButton remote_recog = getActivity().findViewById(R.id.remote_recog);
+        setBounds(R.mipmap.phonemode,local_recog);
+        setBounds(R.mipmap.phonemode,remote_recog);
+        RadioButton glass_remote_recog = getActivity().findViewById(R.id.glass_remote_recog);
+        RadioButton glass_local_recog = getActivity().findViewById(R.id.glass_local_recog);
+        setBounds(R.mipmap.glassmode,glass_remote_recog);
+        setBounds(R.mipmap.glassmode,glass_local_recog);
+        RadioButton shipname_detect = getActivity().findViewById(R.id.shipname_detect);
+        RadioButton shipclass_detect = getActivity().findViewById(R.id.shipclass_detect);
+        setBounds(R.mipmap.ship,shipname_detect);
+        setBounds(R.mipmap.ship,shipclass_detect);
         Activity baseActivity = getActivity();
         Button local_button = baseActivity.findViewById(R.id.local_recog);
         Button remote_button = baseActivity.findViewById(R.id.remote_recog);
@@ -54,6 +69,19 @@ public class ContentfuncActivity extends Fragment {
 
     }
 
+    /**
+     *
+     * @param drawableId  drawableLeft  drawableTop drawableBottom 所用的选择器 通过R.drawable.xx 获得
+     * @param radioButton  需要限定图片大小的radioButton
+     */
+    private void setBounds(int drawableId, RadioButton radioButton) {
+        //定义底部标签图片大小和位置
+        Drawable drawable_news = getResources().getDrawable(drawableId);
+        //当这个图片被绘制时，给他绑定一个矩形 ltrb规定这个矩形  (这里的长和宽写死了 自己可以可以修改成 形参传入)
+        drawable_news.setBounds(0, 0, 95, 95);
+        //设置图片在文字的哪个方向
+        radioButton.setCompoundDrawables(null,null,null, drawable_news);
+    }
 
 
 
