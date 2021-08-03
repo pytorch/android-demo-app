5fdf58723ff4b57b27e5d5b65a01888d38cd9bcc
Jeff Tang <jeffxtang@fb.com>
2021-08-03 11:51:33 -0700

    PR feedback addressed

--- ASLRecognition/app/src/main/java/org/pytorch/demo/aslrecognition/LiveASLRecognitionActivity.java
+++ ASLRecognition/app/src/main/java/org/pytorch/demo/aslrecognition/LiveASLRecognitionActivity.java
@@ -50,77 +50,83 @@ public class LiveASLRecognitionActivity extends org.pytorch.demo.aslrecognition.
         public AnalysisResult(String results) {
             mResults = results;
         }
-        }
-
-        @Override
-        protected int getContentViewLayoutId() {
-            return R.layout.activity_live_asl_recognition;
-        }
-
-        @Override
-        protected TextureView getCameraPreviewTextureView() {
-            mResultView = findViewById(R.id.resultView);
-            return ((ViewStub) findViewById(R.id.asl_recognition_texture_view_stub))
-                    .inflate()
-                    .findViewById(R.id.object_detection_texture_view);
-        }
-
-        @Override
-        protected void applyToUiAnalyzeImageResult(AnalysisResult result) {
-            mResultView.setText(result.mResults);
-            mResultView.invalidate();
-        }
-
-        private Bitmap imgToBitmap(Image image) {
-            Image.Plane[] planes = image.getPlanes();
-            ByteBuffer yBuffer = planes[0].getBuffer();
-            ByteBuffer uBuffer = planes[1].getBuffer();
-            ByteBuffer vBuffer = planes[2].getBuffer();
-
-            int ySize = yBuffer.remaining();
-            int uSize = uBuffer.remaining();
-            int vSize = vBuffer.remaining();
-
-            byte[] nv21 = new byte[ySize + uSize + vSize];
-            yBuffer.get(nv21, 0, ySize);
-            vBuffer.get(nv21, ySize, vSize);
-            uBuffer.get(nv21, ySize + vSize, uSize);
-
-            YuvImage yuvImage = new YuvImage(nv21, ImageFormat.NV21, image.getWidth(), image.getHeight(), null);
-            ByteArrayOutputStream out = new ByteArrayOutputStream();
-            yuvImage.compressToJpeg(new Rect(0, 0, yuvImage.getWidth(), yuvImage.getHeight()), 75, out);
-
-            byte[] imageBytes = out.toByteArray();
-            return BitmapFactory.decodeByteArray(imageBytes, 0, imageBytes.length);
-        }
-
-
-        @Override
-        @WorkerThread
-        @Nullable
-        protected AnalysisResult analyzeImage(ImageProxy image, int rotationDegrees) {
-            if (mModule == null) {
-                try {
-                    mModule = LiteModuleLoader.load(MainActivity.assetFilePath(getApplicationContext(), "asl.ptl"));
-                } catch (IOException e) {
-                    return null;
-                }
-            }
-
-            Bitmap bitmap = imgToBitmap(image.getImage());
-            Matrix matrix = new Matrix();
-            matrix.postRotate(90.0f);
-            bitmap = Bitmap.createBitmap(bitmap, 0, 0, bitmap.getWidth(), bitmap.getHeight(), matrix, true);
-            bitmap = Bitmap.createScaledBitmap(bitmap, MainActivity.SIZE, MainActivity.SIZE, true);
-
-            Pair<Integer, Long> idxTm = MainActivity.bitmapRecognition(bitmap, mModule);
-            int maxScoreIdx = idxTm.first;
-            long inferenceTime = idxTm.second;
-
-            String result = String.valueOf((char)(1 + maxScoreIdx + 64));
-            if (maxScoreIdx == DELETE) result = "DELETE";
-            else if (maxScoreIdx == NOTHING) result = "NOTHING";
-            else if (maxScoreIdx == SPACE) result = "SPACE";
-            return new AnalysisResult(String.format("%s - %dms", result, inferenceTime));
-        }
     }
+
+    @Override
+    protected int getContentViewLayoutId() {
+        return R.layout.activity_live_asl_recognition;
+    }
+
+    @Override
+    protected TextureView getCameraPreviewTextureView() {
+        mResultView = findViewById(R.id.resultView);
+        return ((ViewStub) findViewById(R.id.asl_recognition_texture_view_stub))
+                .inflate()
+                .findViewById(R.id.object_detection_texture_view);
+    }
+
+    @Override
+    protected void applyToUiAnalyzeImageResult(AnalysisResult result) {
+        mResultView.setText(result.mResults);
+        mResultView.invalidate();
+    }
+
+    private Bitmap imgToBitmap(Image image) {
+        Image.Plane[] planes = image.getPlanes();
+        ByteBuffer yBuffer = planes[0].getBuffer();
+        ByteBuffer uBuffer = planes[1].getBuffer();
+        ByteBuffer vBuffer = planes[2].getBuffer();
+
+        int ySize = yBuffer.remaining();
+        int uSize = uBuffer.remaining();
+        int vSize = vBuffer.remaining();
+
+        byte[] nv21 = new byte[ySize + uSize + vSize];
+        yBuffer.get(nv21, 0, ySize);
+        vBuffer.get(nv21, ySize, vSize);
+        uBuffer.get(nv21, ySize + vSize, uSize);
+
+        YuvImage yuvImage = new YuvImage(nv21, ImageFormat.NV21, image.getWidth(), image.getHeight(), null);
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        yuvImage.compressToJpeg(new Rect(0, 0, yuvImage.getWidth(), yuvImage.getHeight()), 75, out);
+
+        byte[] imageBytes = out.toByteArray();
+        return BitmapFactory.decodeByteArray(imageBytes, 0, imageBytes.length);
+    }
+
+
+    @Override
+    @WorkerThread
+    @Nullable
+    protected AnalysisResult analyzeImage(ImageProxy image, int rotationDegrees) {
+        if (mModule == null) {
+            try {
+                mModule = LiteModuleLoader.load(MainActivity.assetFilePath(getApplicationContext(), "asl.ptl"));
+            } catch (IOException e) {
+                return null;
+            }
+        }
+
+        Bitmap bitmap = imgToBitmap(image.getImage());
+        Matrix matrix = new Matrix();
+        matrix.postRotate(90.0f);
+        bitmap = Bitmap.createBitmap(bitmap, 0, 0, bitmap.getWidth(), bitmap.getHeight(), matrix, true);
+        bitmap = Bitmap.createScaledBitmap(bitmap, MainActivity.SIZE, MainActivity.SIZE, true);
+
+        Pair<Integer, Long> idxTm = MainActivity.bitmapRecognition(bitmap, mModule);
+        int maxScoreIdx = idxTm.first;
+        long inferenceTime = idxTm.second;
+
+        String result = String.valueOf((char)(1 + maxScoreIdx + 64));
+        if (maxScoreIdx == DELETE) {
+            result = "DELETE";
+        }
+        else if (maxScoreIdx == NOTHING) {
+            result = "NOTHING";
+        }
+        else if (maxScoreIdx == SPACE) {
+            result = "SPACE";
+        }
+        return new AnalysisResult(String.format("%s - %dms", result, inferenceTime));
+    }
+}
